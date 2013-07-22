dd99403dc75c2d4e22c43598ac9485c203186614
Andrija Petrovic <andrija.hers@gmail.com>
2013-07-22 23:51:23 +0200

    Multiplicates supported

--- shuffler.js
+++ shuffler.js
@@ -1,29 +1,24 @@
 var randomBytes = require('crypto').randomBytes;
 function shuffle(length){
 	var retval = [];
-	try{
-		var codes=[];
-		var map = {};
-		var buf = randomBytes(4*length);
-		for(var i=0; i<length; i++){
-			var code=buf[i*4+0]*256*256*256+buf[i*4+1]*256*256+buf[i*4+2]*256+buf[i*4+3];
-			if(codes.indexOf(code)<0){
-				codes.push(code);
-				map[code]=i;
-			}else{
-				return [];
-			}
-		}
-		//console.log(codes);
-		codes.sort();
-		for(var i in codes){
-			retval.push(map[codes[i]]);
-		}
-	}
-	catch(e){
-		console.log('reshuffling because '+e);
-		return [];
-	}
+  var codes={};
+  var codearray=[];
+  var map = {};
+  var buf = randomBytes(4*length);
+  for(var i=0; i<length; i++){
+    var code=buf[i*4+0]*256*256*256+buf[i*4+1]*256*256+buf[i*4+2]*256+buf[i*4+3];
+    if(!codes[code]){
+      codes[code]=1;
+      codearray.push(code);
+      map[code]=[i];
+    }else{
+      (map[code]).push(i);
+    }
+  }
+  codearray.sort(function(a,b){return a-b;});
+  for(var i in codearray){
+    Array.prototype.push.apply(retval,map[codearray[i]]);
+  }
 	return retval;
 }
 
